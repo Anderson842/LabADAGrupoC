12a1a456fe7facd7e850a858762ec813e76a0637
Anderson Rivera <ariveraqu@unsa.edu.pe>
2021-11-16 14:28:57 -0500

    Ejercicio12

---
+++ Aula5/Q12.py
@@ -0,0 +1,13 @@
+#Q12: What is the time complexity o
+for i in range(10):
+    for j in range(1,10):
+        j=j*2
+        if(j>9):
+            break
+        print(i, j)
+        
+        
+   
+
+
+#La complejidad es O(log n*p)
