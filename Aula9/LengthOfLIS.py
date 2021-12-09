19d710d47be9a99a4f7503894b8c3deb361734fa
Anderson Rivera <ariveraqu@unsa.edu.pe>
2021-12-09 12:30:20 -0500

    Ejercicio 3

---
+++ Aula9/LengthOfLIS.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) if dp else 0
