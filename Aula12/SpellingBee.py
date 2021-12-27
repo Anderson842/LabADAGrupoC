cedf4df32066597caf4531d650a60d91c72f5c1d
Anderson Rivera <ariveraqu@unsa.edu.pe>
2021-12-26 22:31:06 -0500

    SpellingBee

---
+++ Aula12/SpellingBee.py
@@ -0,0 +1,22 @@
+def MatchingWord(word, hexagonLetters):
+    for letter in word:
+        if letter not in hexagonLetters:
+            return False
+
+    return True
+
+
+hexagonLetters = input()
+mandatoryFirstHexagonletter = hexagonLetters[0]
+
+numberOfDictionaryWords = int(input())
+for _ in range(numberOfDictionaryWords):
+    dictionaryWord = input()
+    if len(dictionaryWord) < 4:
+        continue
+
+    if mandatoryFirstHexagonletter not in dictionaryWord:
+        continue
+
+    if MatchingWord(dictionaryWord, hexagonLetters):
+        print(dictionaryWord)
